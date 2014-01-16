16764edb76a210320afd8f0cd87cbf967eb3722f
Daniela <daniela@studioz4.com>
2014-01-16 17:24:32 -0200

    Novo

---
+++ Teste/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using BundleTransformer.Core.Orderers;
+using BundleTransformer.Core.Transformers;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Teste
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.UseCdn = true;
+
+            var cssTransformer = new CssTransformer();
+            var nullOrderer = new NullOrderer();
+           
+            var commonStylesBundle = new Bundle("~/Content/css");
+            commonStylesBundle.Include("~/Content/teste.less");
+            commonStylesBundle.Transforms.Add(cssTransformer);
+            commonStylesBundle.Orderer = nullOrderer;
+            bundles.Add(commonStylesBundle);
+        }
+
+    }
+}
